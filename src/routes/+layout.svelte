4303e64f3e8cd99bfc7cce8ed66ccf84b570f400
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-08-30 19:36:54 -0400

    tab long label clip issue, close button position issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 
 	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
 	let active = 0;
-	let tabs = ['Tab 1', 'Tab 2', 'Tab 3'];
+	let tabs = ['Tab 111111111111111111111111111111', 'Tab 2', 'Tab 3'];
 
 	const addTab = () => (tabs = [...tabs, 'Tab ' + (tabs.length + 1)]);
 	const removeTab = (index: number) => (tabs = tabs.filter((t, i) => i !== index));
@@ -29,11 +29,58 @@
 			<svelte:fragment slot="lead">
 				<strong class="text-xl uppercase">Skeleton</strong>
 			</svelte:fragment>
-			<TabGroup>
+			<TabGroup
+				flex="snap-align-none group flex flex-1 justify-start items-start w-[150px] min-w-[70px] max-w-[150px] overflow-hidden"
+				padding="px-2 py-2"
+				active="border border-b-0"
+				spacing="space-y-0 space-x-1 flex flex-1 justify-start items-start"
+			>
 				{#each tabs as tab, i}
 					<Tab label={tab} bind:group={active} name={tab} value={i}>
-						<svelte:fragment slot="lead">(icon)</svelte:fragment>
-						<span>{tab}</span>
+						<svelte:fragment slot="lead">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="icon icon-tabler icon-tabler-message-plus"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								stroke-width="2"
+								stroke="currentColor"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							>
+								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+								<path d="M8 9h8" />
+								<path d="M8 13h6" />
+								<path
+									d="M12.01 18.594l-4.01 2.406v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5"
+								/>
+								<path d="M16 19h6" />
+								<path d="M19 16v6" />
+							</svg>
+						</svelte:fragment>
+						<span class="clip-tab-title">{tab}</span>
+						<span class="my-close-icon absolute h-0 w-auto left-[-28px] hidden group-hover:block">
+							<button class="btn-icon w-[25px] variant-ringed-tertiary !bg-[darkslateblue] rounded">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="icon icon-tabler icon-tabler-x"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									stroke-width="2"
+									stroke="currentColor"
+									fill="none"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<path d="M18 6l-12 12" />
+									<path d="M6 6l12 12" />
+								</svg>
+							</button>
+						</span>
 					</Tab>
 					<!-- <svelte:fragment slot="panel">
 						<button class="btn variant-outline-warning" type="button" on:click={() => removeTab(i)}
@@ -44,9 +91,27 @@
 			</TabGroup>
 
 			<svelte:fragment slot="trail">
-				<button class="my-add-btn btn variant-filled-primary" type="button" on:click={addTab}
-					>Add Tab</button
-				>
+				<button class="my-add-btn btn variant-filled-primary" type="button" on:click={addTab}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="icon icon-tabler icon-tabler-script-plus"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M17 19h4" />
+						<path
+							d="M14 20h-8a3 3 0 0 1 0 -6h11a3 3 0 0 0 -3 3m7 -3v-8a2 2 0 0 0 -2 -2h-10a2 2 0 0 0 -2 2v8"
+						/>
+						<path d="M19 17v4" />
+					</svg>
+				</button>
 
 				<select class="select" bind:value={active} placeholder="Change Tab">
 					{#each tabs as tab, i}
@@ -86,4 +151,10 @@
 
 <style>
 	/* slotDefault */
+	.clip-tab-title {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		width: inherit;
+	}
 </style>
